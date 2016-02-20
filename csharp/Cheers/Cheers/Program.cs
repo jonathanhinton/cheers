1b54ba66e02d9048083ac8140ffbacfbb6aa2459
Jonathan Hinton <hintonjonathan@gmail.com>
2016-02-20 09:31:13 -0800

    add ToLower and ToUpper methods to match requirements, remove uppercase contingencies for a/an condition

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -22,17 +22,18 @@ namespace Cheers
 
             //print userName to screen after successfull entry
             Console.WriteLine("Hello " + userName);
+            userName = userName.ToLower();
 
             //create charArray from userName
             char[] userNameArray = userName.ToCharArray();
-
+            
             //for loop to print userNameArray
             for (int i = 0; i < userNameArray.Length; i++)
             {
                 //if statement for a/an condition
                 string giveMeA = "Give me a ";
                 string giveMeAn = "Give me an ";
-                if (userNameArray[i] == 'a' || userNameArray[i] == 'e' || userNameArray[i] == 'f' || userNameArray[i] == 'h' || userNameArray[i] == 'i' || userNameArray[i] == 'l' || userNameArray[i] == 'm' || userNameArray[i] == 'n' || userNameArray[i] == 'o' || userNameArray[i] == 'r' || userNameArray[i] == 's' || userNameArray[i] == 'x' || userNameArray[i] == 'A' || userNameArray[i] == 'E' || userNameArray[i] == 'F' || userNameArray[i] == 'H' || userNameArray[i] == 'I' || userNameArray[i] == 'L' || userNameArray[i] == 'M' || userNameArray[i] == 'N' || userNameArray[i] == 'O' || userNameArray[i] == 'R' || userNameArray[i] == 'S' || userNameArray[i] == 'X')
+                if (userNameArray[i] == 'a' || userNameArray[i] == 'e' || userNameArray[i] == 'f' || userNameArray[i] == 'h' || userNameArray[i] == 'i' || userNameArray[i] == 'l' || userNameArray[i] == 'm' || userNameArray[i] == 'n' || userNameArray[i] == 'o' || userNameArray[i] == 'r' || userNameArray[i] == 's' || userNameArray[i] == 'x')
                 {
                     Console.WriteLine(giveMeAn + userNameArray[i] + "...");
                 } else
@@ -42,6 +43,7 @@ namespace Cheers
                // Console.WriteLine("Give me a " + userNameArray[i] + "...");
                 //Console.ReadKey();
             }
+            userName = userName.ToUpper();
             Console.WriteLine(userName + " is GREAT!!!");
 
             //Part Deux
