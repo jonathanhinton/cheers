e59afc3ddca1eab6539297f0209376dedd1293c4
Jonathan Hinton <hintonjonathan@gmail.com>
2016-02-20 08:20:59 -0800

    create charArray from user input, display index 0

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,10 +10,22 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            //declare variable userName to be a string
             string userName;
+
+            //display prompt for user input
             Console.WriteLine("What's your name?");
+
+            //assign userName to the input typed into console
             userName = Console.ReadLine();
+
+            //print userName to screen after successfull entry
             Console.WriteLine("Hello " + userName);
+
+            //create charArray from userName
+            char[] userNameArray = userName.ToCharArray();
+            Console.WriteLine("Give me a " + userNameArray[0]);
+            //keep console open until keypress
             Console.ReadKey();
         }
     }
